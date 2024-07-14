41935789c6ed2e2929897e4fb34aa3582ff7fdae
Manoj Prabaharan <manoj.magento@gmail.com>
2024-07-14 18:01:26 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const header = React.createElement("h1", {class: "helo", id: "his"}, "Hello World From App JS");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
